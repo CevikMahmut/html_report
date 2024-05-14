3ea4cecd6ce5235e746a45445f64dadac284f4e6
Mahmut Çevik <81434434+CevikMahmut@users.noreply.github.com>
2024-05-14 17:27:40 +0300

    initial

---
+++ html_report_test.py
@@ -0,0 +1,20 @@
+import json
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+
+# Verilerinizi yükleyin
+with open('data.json') as f:
+    data = json.load(f)
+
+# Jinja2 ortamını ve şablon dosyasını ayarlayın
+env = Environment(loader=FileSystemLoader('templates'))
+template = env.get_template('report_template.html')
+
+# Şablona verilerinizi geçin
+rendered_html = template.render(date=datetime.now().strftime("%Y-%m-%d"), people=data)
+
+# Render edilmiş HTML'yi bir dosyaya kaydedin
+with open('report.html', 'w') as f:
+    f.write(rendered_html)
+
+print("Rapor oluşturuldu: report.html")
