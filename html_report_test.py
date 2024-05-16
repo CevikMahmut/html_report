6afcfaed5be153a3548c7fe732e9daea383bef80
Mahmut Çevik <cevikmahmut18@gmail.com>
2024-05-16 10:59:42 +0300

    css_path_handled

--- html_report_test.py
+++ html_report_test.py
@@ -7,14 +7,14 @@ with open('./data/data.json') as f:
     data = json.load(f)
 
 # Jinja2 ortamını ve şablon dosyasını ayarlayın
-env = Environment(loader=FileSystemLoader('templates'))
+env = Environment(loader=FileSystemLoader('./templates'))
 template = env.get_template('report_template.html')
 
 # Şablona verilerinizi geçin
 rendered_html = template.render(date=datetime.now().strftime("%Y-%m-%d"), people=data)
 
 # Render edilmiş HTML'yi bir dosyaya kaydedin
-with open('report.html', 'w') as f:
+with open('./report/report.html', 'w') as f:
     f.write(rendered_html)
 
 print("Rapor oluşturuldu: report.html")
